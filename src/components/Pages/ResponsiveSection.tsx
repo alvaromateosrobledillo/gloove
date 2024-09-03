022fa602ef8d4ee8900897294ee6efa45702d1b7
Alvaro <alvaromar988@gmail.com>
2024-09-03 07:55:15 +0200

    diseño

--- src/components/Pages/ResponsiveSection.tsx
+++ src/components/Pages/ResponsiveSection.tsx
@@ -9,16 +9,16 @@ const ResponsiveSection: React.FC = () => {
     const updateVideoSrc = () => {
       const width = window.innerWidth;
       if (width >= 1920) {
-        setVideoSrc("/colaboradores/RuletaO2.mp4"); // Monitores grandes
+        setVideoSrc("./colaboradores/RuletaO2.mp4"); // Monitores grandes
         setIsLargeScreen(true);
       } else if (width >= 1024) {
-        setVideoSrc("/colaboradores/RuletaO2.mp4"); // PC
+        setVideoSrc("./colaboradores/RuletaO2.mp4"); // PC
         setIsLargeScreen(true);
       } else if (width >= 768) {
-        setVideoSrc("/colaboradores/RuletaT.mp4"); // Tablets
+        setVideoSrc("./colaboradores/RuletaT.mp4"); // Tablets
         setIsLargeScreen(false);
       } else {
-        setVideoSrc("/colaboradores/RuletaM.mp4"); // Móviles
+        setVideoSrc("./colaboradores/RuletaM.mp4"); // Móviles
         setIsLargeScreen(false);
       }
     };
