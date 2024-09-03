de39fb7e6719921e855123a65af6bfd536cf2e2e
Alvaro <alvaromar988@gmail.com>
2024-09-03 08:28:10 +0200

    source

--- src/components/Pages/ResponsiveSection.tsx
+++ src/components/Pages/ResponsiveSection.tsx
@@ -1,30 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
+// Definir rutas de los videos
+const videos = {
+  largeScreen: "/colaboradores/RuletaO2.mp4",
+  pc: "/colaboradores/RuletaO2.mp4",
+  tablet: "/colaboradores/RuletaT.mp4",
+  mobile: "/colaboradores/RuletaM.mp4",
+};
+
 const ResponsiveSection: React.FC = () => {
-  const [videoSrc, setVideoSrc] = useState("/colaboradores/RuletaM.mp4");
+  const [videoSrc, setVideoSrc] = useState(videos.mobile);
   const [isLargeScreen, setIsLargeScreen] = useState(false);
 
   useEffect(() => {
     const updateVideoSrc = () => {
       const width = window.innerWidth;
       if (width >= 1920) {
-        setVideoSrc("/colaboradores/RuletaO2.mp4"); // Monitores grandes
+        setVideoSrc(videos.largeScreen); // Monitores grandes
         setIsLargeScreen(true);
       } else if (width >= 1024) {
-        setVideoSrc("/colaboradores/RuletaO2.mp4"); // PC
+        setVideoSrc(videos.pc); // PC
         setIsLargeScreen(true);
       } else if (width >= 768) {
-        setVideoSrc("/colaboradores/RuletaT.mp4"); // Tablets
+        setVideoSrc(videos.tablet); // Tablets
         setIsLargeScreen(false);
       } else {
-        setVideoSrc("/colaboradores/RuletaM.mp4"); // Móviles
+        setVideoSrc(videos.mobile); // Móviles
         setIsLargeScreen(false);
       }
     };
 
-    updateVideoSrc(); // Ejecutar al montar el componente
-    window.addEventListener("resize", updateVideoSrc); // Ejecutar al redimensionar la ventana
+    updateVideoSrc();
+    window.addEventListener("resize", updateVideoSrc);
 
     return () => window.removeEventListener("resize", updateVideoSrc);
   }, []);
@@ -32,21 +40,20 @@ const ResponsiveSection: React.FC = () => {
   return (
     <section className="min-h-screen bg-[#F6F7F5] flex flex-col justify-center items-center">
       {isLargeScreen ? (
-        // Layout para PC y Monitores
         <div className="grid grid-cols-3 gap-0 w-full h-screen">
-          {/* Video - Ocupa 2/3 en pantallas grandes */}
           <div className="col-span-2 h-full relative overflow-hidden">
             <video
               className="w-full h-full object-cover absolute inset-0"
-              src={videoSrc}
               autoPlay
               muted
               loop
               playsInline
-            />
+            >
+              <source src={videoSrc} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           </div>
 
-          {/* Título y Subtítulo - Ocupa 1/3 a la derecha */}
           <div className="flex flex-col justify-center items-center text-center lg:text-left p-8 bg-[#F6F7F5] h-full">
             <motion.h2
               className="text-3xl md:text-5xl font-extrabold text-gloovePrimary-dark mb-4"
@@ -67,9 +74,7 @@ const ResponsiveSection: React.FC = () => {
           </div>
         </div>
       ) : (
-        // Layout para Móviles y Tablets
         <div className="flex flex-col justify-between items-center w-full h-full">
-          {/* Título y Subtítulo arriba */}
           <div className="flex flex-col justify-center items-center text-center p-8 w-full">
             <motion.h2
               className="text-3xl md:text-4xl font-extrabold text-gloovePrimary-dark mb-4"
@@ -89,16 +94,17 @@ const ResponsiveSection: React.FC = () => {
             </motion.p>
           </div>
 
-          {/* Video abajo */}
           <div className="w-full h-2/3">
             <video
               className="w-full h-full object-cover"
-              src={videoSrc}
               autoPlay
               muted
               loop
               playsInline
-            />
+            >
+              <source src={videoSrc} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           </div>
         </div>
       )}
